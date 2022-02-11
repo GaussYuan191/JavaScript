e490448f50e756c11be044ba451dfb0bfb253ee9
GaussYuan191 <1915059302@qq.com>
2022-02-11 13:29:20 +0800

    新增 节流

--- src/throttle.js
+++ src/throttle.js
@@ -1,10 +1,24 @@
 // 节流 一段时间， 函数只执行一次，高频事件触发，节流会稀释函数的执行频率
 // 基于时间戳实现
-function throttle (fn, time) {
-    let pre = 0;
-    return function (...args) {
-        if (Date.now - pre > time) {
-            fn.apply(this, args)
-        }
+function throttle(fn, time) {
+  let pre = 0;
+  return function (...args) {
+    let now = Date.now();
+    if (now - pre > time) {
+      fn.apply(this, args);
+      pre = now;
     }
+  };
+}
+// 基于定时器
+function throttle(fn, time) {
+  let timer = null;
+  return function (...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+        timer = null;
+      }, time);
+    }
+  };
 }
